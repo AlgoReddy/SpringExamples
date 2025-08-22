34f550bf1adbe15bbb9affaf10c6b76df23fa069
algoreddy <helloyash99@gmail.com>
2025-08-22 17:46:34 +0530

    hey i created a whole new directory for working on the projects of the spring:

---
+++ ColoursApplication/src/main/java/com/maven/ColoursApplication/ColoursApplication.java
@@ -0,0 +1,13 @@
+package com.maven.ColoursApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColoursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColoursApplication.class, args);
+	}
+
+}
---
+++ ColoursApplication/src/test/java/com/maven/ColoursApplication/ColoursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.ColoursApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColoursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
