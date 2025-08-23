6b5f7b680e28bd8aa3e3281c9fe63cf02d0ccd2d
algoreddy <helloyash99@gmail.com>
2025-08-23 18:05:31 +0530

    successfully implemented the objects using the beans

--- 0_no_dependency_injection/colours/src/main/java/com/deviro/maven/config/PrinterConfig.java
+++ 0_no_dependency_injection/colours/src/main/java/com/deviro/maven/config/PrinterConfig.java
@@ -1,1 +1,36 @@
+package com.deviro.maven.config;
 
+import com.deviro.maven.services.BluePrinter;
+import com.deviro.maven.services.ColourPrinter;
+import com.deviro.maven.services.GreenPrinter;
+import com.deviro.maven.services.RedPrinter;
+import com.deviro.maven.services.impl.ColourPrinterImpl;
+import com.deviro.maven.services.impl.EnglishBluePrinter;
+import com.deviro.maven.services.impl.EnglishGreenPrinter;
+import com.deviro.maven.services.impl.EnglishRedPrinter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrinterConfig {
+  @Bean
+  public RedPrinter redPrinter() {
+    return new EnglishRedPrinter();
+  }
+
+  @Bean
+  public GreenPrinter greenPrinter() {
+    return new EnglishGreenPrinter();
+  }
+
+  @Bean
+  public BluePrinter bluePrinter() {
+    return new EnglishBluePrinter();
+  }
+
+  @Bean
+  public ColourPrinter colourPrinter(
+      RedPrinter redPrinter, GreenPrinter greenPrinter, BluePrinter bluePrinter) {
+    return new ColourPrinterImpl(bluePrinter, greenPrinter, redPrinter);
+  }
+}
