cc1166415aff20f879431808a309c5f441447f19
algoreddy <helloyash99@gmail.com>
2025-08-26 02:20:19 +0530

    implemented the restcontroller , resquestmapping , requestbody

---
+++ webapplication/src/main/java/com/algoreddy/webapplication/HomeController.java
@@ -0,0 +1,19 @@
+package com.algoreddy.webapplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HomeController {
+@RequestMapping("/")
+public String greet(){
+    return "welcome to the yeswanth channel";
+}
+@RequestMapping("/about")
+public String  about(){
+    return " i don't simply learn , i build";
+}
+
+}
